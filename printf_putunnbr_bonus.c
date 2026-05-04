f3ddef733fba241b342fcf1da3c56b69cd2341f6
Luis Palomino Morón <lpalominom4c@gmail.com>
2026-05-04 12:15:46 +0200

    Bonus: First part

--- printf_putunnbr_bonus.c
+++ printf_putunnbr_bonus.c
@@ -16,24 +16,49 @@ static int	calc_size(unsigned int nbr)
 {
 	int	len;
 
-	len = 0;
-	if (nbr == 4294967295)
-		return (10);
-	if (nbr >= 10)
-		len += calc_size(nbr / 10);
-	len++;
+	len = 1;
+	while (nbr >= 10)
+	{
+		nbr /= 10;
+		len++;
+	}
 	return (len);
+}
+
+static int	calc_total_len(unsigned int nbr, t_format format,
+	int *amount_zero)
+{
+	int	size;
+	int	total_len;
+
+	size = calc_size(nbr);
+	*amount_zero = 0;
+	if (format.dot && format.precision > size)
+		*amount_zero = format.precision - size;
+	total_len = size + *amount_zero;
+	if (format.dot && format.precision == 0 && nbr == 0)
+		total_len = 0;
+	return (total_len);
 }
 
 int	printf_putunnbr_bonus(unsigned int nbr, t_format format)
 {
 	int	printed;
+	int	amount_zero;
+	int	total_len;
 
 	printed = 0;
+	total_len = calc_total_len(nbr, format, &amount_zero);
 	if (!format.minus)
-		printed += manage_padding(format.width, calc_size(nbr));
-	printed += printf_putunnbr(nbr);
+		printed += manage_padding(format.width, total_len);
+	while (amount_zero > 0)
+	{
+		printed += printf_putchar('0');
+		amount_zero--;
+	}
+	if (!(format.dot && format.precision == 0 && nbr == 0))
+		printed += printf_putunnbr(nbr);
 	if (format.minus)
-		printed += manage_padding(format.width, calc_size(nbr));
+		printed += manage_padding(format.width, total_len);
 	return (printed);
 }
